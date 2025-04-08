f17ecb1fd82ab9396807fbc3a6292bb4d8d940d1
Jose Ortega <jmortegafreire@gmail.com>
2025-04-08 10:11:08 -0400

    base usb terminal app

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# rf433_industrial_joystick
+# 433 Industrial Joystick's Android Interface
+
+Android app to interface with the 433 arduino based industrial joystick
+
+## Credits
+
+The app uses as a base the [SimpleUsbTerminal](https://github.com/kai-morich/SimpleUsbTerminal) Android app.
---
+++ app/src/main/java/cl/jmortegaf/rf433_industrial_joystick/SerialListener.java
@@ -0,0 +1,11 @@
+package cl.jmortegaf.rf433_industrial_joystick;
+
+import java.util.ArrayDeque;
+
+interface SerialListener {
+    void onSerialConnect      ();
+    void onSerialConnectError (Exception e);
+    void onSerialRead         (byte[] data);                // socket -> service
+    void onSerialRead         (ArrayDeque<byte[]> datas);   // service -> UI thread
+    void onSerialIoError      (Exception e);
+}
